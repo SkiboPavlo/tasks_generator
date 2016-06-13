ae883e942842579c3379dd42eaaf922bd6471b64
Pavlo <skibo@i.ua>
2016-06-13 16:42:44 +0300

    add relationship

--- db/migrate/20160607211154_add_timestamps_to_tasks.rb
+++ db/migrate/20160607211154_add_timestamps_to_tasks.rb
@@ -1,4 +1,4 @@
-class AddTimestampsToTasks < ActiveRecord::Migration
+ class AddTimestampsToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :created_at, :timestamp, null: false
     add_column :tasks, :updated_at, :timestamp, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607211154) do
+ActiveRecord::Schema.define(version: 20160613112423) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", default: '2016-06-07 21:21:43', null: false
     t.datetime "updated_at", default: '2016-06-07 21:21:43', null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
